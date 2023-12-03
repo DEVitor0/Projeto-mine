d51201b5b55260b55db5b2f2792b33a0e135e142
Vitor Moreira <133174280+DEVitor0@users.noreply.github.com>
2023-12-03 09:03:59 -0300

    menu de navegação das noticias, eventos, regras

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
-// Selecionar o botão do menu hamburguer
+//menu hamburguer
 const hamburgerBtn = document.getElementById('hamburger-btn');
 const menuList = document.querySelector('.menu-list');
 
-// Adicionar evento de clique ao botão do menu hamburguer
 hamburgerBtn.addEventListener('click', function() {
   this.classList.toggle('active'); // Adicionar ou remover a classe 'active' ao botão
   menuList.classList.toggle('active'); // Adicionar ou remover a classe 'active' ao menu
 });
+
+
