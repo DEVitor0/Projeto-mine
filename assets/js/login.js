7dcb178271d832a04c4253e83ffef9330124bc11
Vitor Moreira <133174280+DEVitor0@users.noreply.github.com>
2023-12-04 10:50:21 -0300

    criação e inicio da tela de login

---
+++ assets/js/login.js
@@ -0,0 +1,9 @@
+//menu hamburguer
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const menuList = document.querySelector('.menu-list');
+
+hamburgerBtn.addEventListener('click', function() {
+  this.classList.toggle('active'); 
+  menuList.classList.toggle('active'); 
+});
+
