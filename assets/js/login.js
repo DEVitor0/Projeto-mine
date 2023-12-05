a9b9ae74919a78f80dcd32e4c46e3dcf91d9aaf3
Vitor Moreira <133174280+DEVitor0@users.noreply.github.com>
2023-12-04 22:38:28 -0300

    criação do script de login

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,3 +7,23 @@ hamburgerBtn.addEventListener('click', function() {
   menuList.classList.toggle('active'); 
 });
 
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const usuario = document.getElementById('user').value;
+  const senha = document.getElementById('senha').value;
+
+  if (usuario === 'Kaua' && senha === 'Lipskk') {
+      window.location.href = 'LojaKaua.html';
+  } else if (usuario === 'Kevin' && senha === 'kevin102030') {
+      window.location.href = 'LojaKevin.html';
+  } else if (usuario === 'Eduardo' && senha === '1109') {
+      window.location.href = 'Eduardo.html';
+  } else if (usuario === 'João' && senha === 'jpgraciano2701') {
+      window.location.href = 'Joao.html';
+  } else if (usuario === 'Andre' && senha === 'DodôTop') {
+    window.location.href = 'Andre.html' ;
+  } else {
+      alert('Credenciais inválidas. Tente novamente.');
+  }
+});
