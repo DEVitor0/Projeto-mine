fcb4086c673e5052b5a039e9eaf3266dc8bf95da
Vitor Moreira <133174280+DEVitor0@users.noreply.github.com>
2023-12-05 00:07:33 -0300

    criação do html da loja e css do menu dela

--- assets/js/login.js
+++ assets/js/login.js
@@ -14,15 +14,15 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   const senha = document.getElementById('senha').value;
 
   if (usuario === 'Kaua' && senha === 'Lipskk') {
-      window.location.href = 'LojaKaua.html';
+      window.location.href = 'LojaKaua.html'; //!REVER OS CAMINHOS DE CADA ITEM
   } else if (usuario === 'Kevin' && senha === 'kevin102030') {
-      window.location.href = 'LojaKevin.html';
+      window.location.href = 'LojaKevin.html'; //!REVER OS CAMINHOS DE CADA ITEM
   } else if (usuario === 'Eduardo' && senha === '1109') {
-      window.location.href = 'Eduardo.html';
+      window.location.href = 'Eduardo.html'; //!REVER OS CAMINHOS DE CADA ITEM
   } else if (usuario === 'João' && senha === 'jpgraciano2701') {
-      window.location.href = 'Joao.html';
+      window.location.href = 'Joao.html'; //!REVER OS CAMINHOS DE CADA ITEM
   } else if (usuario === 'Andre' && senha === 'DodôTop') {
-    window.location.href = 'Andre.html' ;
+    window.location.href = 'Andre.html' ; //!REVER OS CAMINHOS DE CADA ITEM
   } else {
       alert('Credenciais inválidas. Tente novamente.');
   }
---
+++ assets/js/loja.js
@@ -0,0 +1,7 @@
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const menuList = document.querySelector('.menu-list');
+
+hamburgerBtn.addEventListener('click', function() {
+  this.classList.toggle('active'); 
+  menuList.classList.toggle('active'); 
+});
