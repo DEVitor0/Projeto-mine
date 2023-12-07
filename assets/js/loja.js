bdae7df7d0800cd3662263fbde746a09d707875f
Vitor Moreira <133174280+DEVitor0@users.noreply.github.com>
2023-12-07 11:23:05 -0300

    script da atualização de preços

--- assets/js/loja.js
+++ assets/js/loja.js
@@ -6,3 +6,42 @@ hamburgerBtn.addEventListener('click', function() {
   menuList.classList.toggle('active'); 
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  const pedregulhoQuantidade = document.getElementById('pedregulho');
+  const precoPedregulho = document.getElementById('preço-pedregulho');
+
+  pedregulhoQuantidade.addEventListener('click', function () {
+    switch (this.textContent) {
+      case '64X':
+        this.textContent = '32X';
+        atualizarPreco(32);
+        break;
+      case '32X':
+        this.textContent = '18X';
+        atualizarPreco(18);
+        break;
+      case '18X':
+        this.textContent = '9X';
+        atualizarPreco(9);
+        break;
+      case '9X':
+        this.textContent = '64X';
+        atualizarPreco(64);
+        break;
+      default:
+        break;
+    }
+  });
+
+  function atualizarPreco(novaQuantidade) {
+    const precosPorQuantidade = {
+      64: '$8',
+      32: '$4',
+      18: '$2',
+      9: '$1'
+    };
+
+    const novoPreco = precosPorQuantidade[novaQuantidade];
+    precoPedregulho.textContent = novoPreco;
+  }
+});
